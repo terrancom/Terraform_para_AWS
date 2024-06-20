41295d83a839eeab32b2a91bc272636c85714809
Alberto Juan Gabino <35282567+webonauta@users.noreply.github.com>
2024-06-19 22:48:37 -0600

    Se agregan etiquetas para los buckets

--- Buckets/bucket.tf
+++ Buckets/bucket.tf
@@ -1,11 +1,29 @@
 resource "aws_s3_bucket" "Respaldo_docker_images" {
     bucket = "respaldo-para-imagenes-docker"
+
+    tags = {
+        Owner = "Alberto"
+        Environment = "Dev"
+        Office = "DevOps"
+    }
 }
 
 resource "aws_s3_bucket" "Respaldo_binarios" {
     bucket = "respaldo-para-binarios-linux"
+
+    tags = {
+        Owner = "Alberto"
+        Environment = "Dev"
+        Office = "Cloud"
+    }
 }
 
 resource "aws_s3_bucket" "Documentacion_proyectos" {
     bucket = "documentos-de-proyectos-internos"
+
+    tags = {
+        Owner = "Alberto"
+        Environment = "QA"
+        Office = "QA"
+    }
 }
